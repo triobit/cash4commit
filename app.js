2eae08c7272e4c011016a49881b38c544dbc9876
Sebastian Malek <malek@triobit.net>
2017-12-16 14:23:45 +0100

    add pug

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-	res.json({});
+	res.render('index');
 });
 
 app.listen(3000, () => {
